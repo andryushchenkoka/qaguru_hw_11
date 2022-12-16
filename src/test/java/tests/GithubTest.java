c655b525103a40a189410553e7e2a7f02afb4fb7
KirillAndryushchenko <kirill2goo@gmail.com>
2022-12-16 15:47:06 +0300

    Добавлен отчет (для себя)

--- src/test/java/tests/GithubTest.java
+++ src/test/java/tests/GithubTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -7,20 +8,29 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class GithubTest extends BaseTest {
 
     @Test
+    @DisplayName("На странице SoftAssertions есть пример кода для JUnit5")
     public void githubTest() {
 
-        // - Откройте страницу Selenide в Github
-        open("selenide/selenide");
-        // - Перейдите в раздел Wiki проекта
-        $("#wiki-tab").click();
-        // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-        $("div[id = 'wiki-pages-box']").$(withText("more pages")).click();
-        $("div[id = 'wiki-pages-box']").$(byText("SoftAssertions")).shouldBe(visible).click();
-        // - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-        $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("JUnit5"));
+        step("Открыть страницу Selenide в Github", () -> {
+            open("selenide/selenide");
+        });
+
+        step("Перейти в раздел Wiki", () -> {
+            $("#wiki-tab").click();
+        });
+
+        step("В боковом меню есть раздел SoftAssertions", () -> {
+            $("div[id = 'wiki-pages-box']").$(withText("more pages")).click();
+            $("div[id = 'wiki-pages-box']").$(byText("SoftAssertions")).shouldBe(visible).click();
+        });
+
+        step("На странице SoftAssertions есть пример кода для JUnit5", () -> {
+            $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("JUnit5"));
+        });
     }
 }
